72b32d2e6b570a85848937a7fa71edca1f3981bd
Isaac <isaac.keninger@gmail.com>
2026-05-05 10:37:33 -0500

    fix encoding issue through local terminal

--- CodeCoach/app/CodeCoachAgent/test_local.py
+++ CodeCoach/app/CodeCoachAgent/test_local.py
@@ -1,5 +1,12 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+sys.stderr.reconfigure(encoding='utf-8')
+
+import os
+os.environ['PYTHONUTF8'] = '1' # this makes os encoding work, known aws problem w/ windows
+
 from strands import Agent
-from model.load import load_model
+from model.load import load_model # allow for utf encoding fix
 
 agent = Agent(
     model=load_model(),
@@ -8,4 +15,4 @@ agent = Agent(
 
 prompt = input("Enter code or question: ")
 response = agent(prompt)
-print(response)
+print(response,)
